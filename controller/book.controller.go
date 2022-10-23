93eaddfb537542482a052cd8c36f0eb3c2990d1a
farhan <18220015@std.stei.itb.ac.id>
2022-10-23 12:42:36 +0700

    added delete method

--- controller/book.controller.go
+++ controller/book.controller.go
@@ -28,6 +28,37 @@ func BookController(
 	bookRouter.PUT("/:id", func(c *gin.Context) {
 		UpdateBookById(c, db)
 	})
+	bookRouter.DELETE("/:id", func(c *gin.Context) {
+		DeleteBookById(c, db)
+	})
+}
+
+func DeleteBookById(c *gin.Context, db *sql.DB) {
+	id := c.Param("id")
+	query := "DELETE FROM books WHERE id = ?;"
+	stmt, err := db.PrepareContext(c, query)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	res, err := stmt.ExecContext(c, id)
+	if err != nil {
+		panic(err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil || rows <= 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"msg":    err,
+			"status": http.StatusNotFound,
+		})
+		panic(err)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"msg":           "Buku berhasil dihapus",
+		"status":        http.StatusOK,
+		"affected-rows": rows,
+	})
+
 }
 
 func UpdateBookById(c *gin.Context, db *sql.DB) {
